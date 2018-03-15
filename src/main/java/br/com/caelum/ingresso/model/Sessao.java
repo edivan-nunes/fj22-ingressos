1c3b63c7362d8061fe0dbd1da5909ab7a48e5dee
edivan-nunes <edivan.nunes91@gmail.com>
2018-03-15 18:17:22 -0300

    #Commit Atualizado com alterações nas Classes.

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -2,12 +2,29 @@ package br.com.caelum.ingresso.model;
 
 import java.time.LocalTime;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+@Entity
 public class Sessao {
 	
+	@Id
+	@GeneratedValue
 	private Integer id;
-	private LocalTime horario;
+	@ManyToOne
 	private Sala sala;
+	@ManyToOne
 	private Filme filme;
+	
+	private LocalTime horario;
+	
+	public Sessao()	{
+		
+	}
 	
 	public Sessao(LocalTime horario, Filme filme, Sala sala)	{
 		this.horario = horario;
@@ -19,7 +36,37 @@ public class Sessao {
 	public LocalTime getHorarioTermino()	{
 		return this.horario.plusMinutes(filme.getDuracao().toMinutes());
 	}
-	
-	//criar getters e setters
 
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public LocalTime getHorario() {
+		return horario;
+	}
+
+	public void setHorario(LocalTime horario) {
+		this.horario = horario;
+	}
+
+	public Sala getSala() {
+		return sala;
+	}
+
+	public void setSala(Sala sala) {
+		this.sala = sala;
+	}
+
+	public Filme getFilme() {
+		return filme;
+	}
+
+	public void setFilme(Filme filme) {
+		this.filme = filme;
+	}
+	
 }
